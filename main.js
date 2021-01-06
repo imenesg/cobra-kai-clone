c4d9fb59ebc03f1538827a42ddccc48ebceb1f07
Isaac Alves <isaac.alves0720@gmail.com>
2021-01-06 20:50:11 -0300

    5th commit

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ bleep.src = "click-sound.mp3";
 
 function cobra(){
     bleep.play();
+    const btnScroollToBottom = document.querySelector("#cobra");
+    window.scrollTo(4827, 4827);
+    // btnScroollToBottom.addEventListener("click", function (){
+    //     $("html", "body").animate({ scrollTop: 10000 }, "slow");
+    // });
 }
 
 sensei = () =>{
