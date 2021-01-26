1c18000c20ab32923dbe8e8cb8626f944ebd4cdb
Isaac Alves <isaac.alves0720@gmail.com>
2021-01-25 22:15:35 -0300

    9th commit

--- assets/public/scripts/js/main.js
+++ assets/public/scripts/js/main.js
@@ -1,9 +1,9 @@
 // Cobra Kai.js
 var bleep = new Audio();
-bleep.src = "cobra-sound.mp3";
+bleep.src = "/assets/public/sound/cobra-sound.mp3";
 
 function cobraRedirect(){
-    document.cobra.src='img.svg';
+    document.cobra.src='/assets/public/img/img.svg';
     bleep.play();
     // window.location.href="#sign-in";
     // window.scrollTo(4687, 4687)
