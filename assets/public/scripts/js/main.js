98a7cd4b825598ada4fdec70539a3969c3d4339e
Isaac Alves <isaac.alves0720@gmail.com>
2021-02-09 23:08:13 -0300

    13th commit - I added the setTimeout function in the picture

--- assets/public/scripts/js/main.js
+++ assets/public/scripts/js/main.js
@@ -5,6 +5,12 @@ bleep.src = "/assets/public/sound/cobra-sound.mp3";
 function cobraRedirect(){
     document.cobra.src='/assets/public/img/img.svg';
     bleep.play();
-    // window.location.href="#sign-in";
-    // window.scrollTo(4687, 4687)
+
+    setTimeout(
+        function() {
+           window.location.href="#sign-in";
+        //    window.scrollTo(4297, 4297)
+        }.bind(this),
+        700
+    );
 }
