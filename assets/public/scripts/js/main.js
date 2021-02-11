46008e47bb436043aa24c0dec5e1f43fe00ff9af
Isaac Alves <isaac.alves0720@gmail.com>
2021-02-11 17:55:36 -0300

    14th commit

--- assets/public/scripts/js/main.js
+++ assets/public/scripts/js/main.js
@@ -1,16 +1,29 @@
-// Cobra Kai.js
-var bleep = new Audio();
-bleep.src = "/assets/public/sound/cobra-sound.mp3";
-
 function cobraRedirect(){
+    const bleep = new Audio();
+    bleep.src = "/assets/public/sound/cobra-sound.mp3";
     document.cobra.src='/assets/public/img/img.svg';
     bleep.play();
 
     setTimeout(
         function() {
-           window.location.href="#sign-in";
-        //    window.scrollTo(4297, 4297)
+           window.location.href="#sign-in"; //window.scrollTo(4297, 4297)
         }.bind(this),
-        700
+        577
     );
+}
+
+function menuHamburger(){
+
+}
+
+function modalPopUp(){
+}
+
+function modalVideoPopUp(){
+}
+
+function punch(){
+    const punchSound = new Audio();
+    punchSound.src = "/assets/public/sound/punch-sound.mp3";
+    punchSound.play();
 }
